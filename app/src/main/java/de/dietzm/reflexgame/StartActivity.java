86a0970efce95ebbe8177c0be12a61308a6ed26f
Michael DIetz <michael.dtz@gmail.com>
2016-11-10 07:29:42 +0100

    Changes

--- app/src/main/java/de/dietzm/reflexgame/StartActivity.java
+++ app/src/main/java/de/dietzm/reflexgame/StartActivity.java
@@ -25,19 +25,7 @@ public class StartActivity extends AppCompatActivity {
 
     public void registerPlayer(View view){
 
-        EditText playerInput = (EditText) findViewById(R.id.playerInput);
-        SeekBar levelSeekbar = (SeekBar) findViewById(R.id.levelSeekbar);
-
-        String playerName = playerInput.getText().toString();
-        int level = levelSeekbar.getProgress();
-
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        SharedPreferences.Editor editor = sp.edit();
-
-        editor.putString("PLAYER", playerName);
-        editor.putInt("LEVEL", level);
-
-        editor.commit();
+        
 
     }
 }
